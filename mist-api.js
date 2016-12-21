dda786595564e9594bb102d508f490ff786ba739
André Kaustell <andre.kaustell@controlthings.fi>
2016-12-21 18:39:15 +0200

    Playing around with several mist-node-api based devices to enable coercion.

--- mist-api.js
+++ mist-api.js
@@ -14,13 +14,38 @@ mist.request('mist.listServices', [], function(err, data) {
     }
     */
    
-    var id = mist.request('control.follow', [data[2]], function(err, data) {
+    var follow = mist.request('control.follow', [data[2]], function(err, data) {
         console.log("Follow update:", data);
     });
     
     setTimeout(function() {
-        mist.requestCancel(id);
+        mist.requestCancel(follow);
     }, 2000);
+
+    //mist.request('control.write', [data[4], 'button1', true], function(err, data) {
+    //    console.log("Follow update:", data);
+    //});
+    
+    /*
+    var to;
+    var from;
+    var fromEpid;
+    var fromOpts = { type: 'direct', interval: 'change' };
+    var toEpid;
+    var toOpts = { type: 'write' };
+    */
+      
+    mist.request('control.requestMapping', [data[4] ,data[2], 'button1', { type: 'direct', interval: 'change' }, 'button1', { type: 'write' }], function(err, data) {
+        console.log("Follow update:", data);
+    });
+    
+    //mist.wish('identity.list', [], function(err, identities) { });
+    
+    /*
+    mist.request('manage.user.ensure', [data[2], { ensure: "node.js" }], function(err, data) {
+        console.log("manage.user.ensure response:", data);
+    });
+    */
 
 });
 
