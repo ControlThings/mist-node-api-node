36a472b31254ad39ecc36fd866039ae1e770b07f
André Kaustell <andre.kaustell@controlthings.fi>
2017-02-06 18:21:11 +0200

    Improved testing framework and wrote some tests.

--- tools/test-suit.js
+++ tools/test-suit.js
@@ -23,36 +23,60 @@ function done() {
             console.log('\x1b[37mnode>', data.toString().trim());
         });
 
-        node.on('exit', function(code, signal) {
+        node.on('exit', (code, signal) => {
             console.log('\x1b[36mnode> Exited with code:', signal ? signal : code);
         });
 
-        console.log('Starting test.');
-        var test = child.spawn('../node_modules/mocha/bin/mocha', ['-c']);
-
-        test.on('error', (err) => {
-            console.log('\x1b[36mtest> Failed to start test process.');
-        });
-
-        test.stdout.on('data', (data) => {
-            console.log('\x1b[36mtest>', data.toString().trim());
-        });
-        
-        test.stderr.on('data', (data) => {
-            console.log('\x1b[36mtest>', data.toString().trim());
-        });
-        
-        test.on('exit', function(code, signal) {
-            console.log('\x1b[36mtest> Exited with code:', code, signal);
-            if( code === 0 ) {
-                console.log('\x1b[35mTest run completed successfully in '+(Date.now()-testStartTime)+'ms');
+        function run(list) {
+            if (list.length > 0) {
+                var file = list.pop();
+            } else {
+                console.log("We're all done.", list);
+                node.kill();
+                core.kill();
+                return;
             }
-            node.kill();
-            core.kill();
+
+            console.log('Starting test.');
+            var test = child.spawn('../node_modules/mocha/bin/mocha', ['-c', './test/'+file]);
+
+            test.on('error', (err) => {
+                console.log('\x1b[36mtest> Failed to start test process.');
+            });
+
+            test.stdout.on('data', (data) => {
+                console.log('\x1b[36mtest>', data.toString().trim());
+            });
+
+            test.stderr.on('data', (data) => {
+                console.log('\x1b[36mtest>', data.toString().trim());
+            });
+
+            test.on('exit', (code, signal) => {
+                console.log('\x1b[36mtest> Exited with code:', code, signal);
+                if( code === 0 ) {
+                    console.log('\x1b[35mTest run completed successfully in '+(Date.now()-testStartTime)+'ms');
+                }
+                
+                process.nextTick(function() { run(list); });
+            });            
+        }
+
+        var list = [];
+        const testFolder = './test/';
+        const fs = require('fs');
+        fs.readdir(testFolder, (err, files) => {
+            files.forEach(file => {
+                list.push(file);
+            });
+            
+            run(list);
         });
+        
+        
     }
     
-    var coreTimeout = setTimeout(function() { running(); }, 200);
+    var coreTimeout = setTimeout(() => { running(); }, 200);
     
     core.on('error', (err) => {
         console.log('\x1b[35mwish> Failed to start wish-core process.');
@@ -67,11 +91,16 @@ function done() {
         console.log('wish>', data.toString().trim());
     });
     
-    core.on('exit', function(code) {
+    core.on('exit', (code) => {
         console.log("wish exited with code:", code);
         clearTimeout(coreTimeout);
     });
 }
+
+try {
+    //fs.unlinkSync('./env/wish_hostid.raw');
+    //fs.unlinkSync('./env/wish_id_db.bson');
+} catch (e) {}
 
 https.get(wishBinaryUrl, (res) => {
     //console.log('statusCode:', res.statusCode);
@@ -83,7 +112,7 @@ https.get(wishBinaryUrl, (res) => {
     var file = fs.createWriteStream(fileName);
 
     if (res.statusCode === 200) {
-        file.on('close', function() { console.log('Downloaded Wish binary '+(Date.now()-downloadTime)+'ms'); fs.chmodSync(fileName, '755'); done(); });
+        file.on('close', () => { console.log('Downloaded Wish binary '+(Date.now()-downloadTime)+'ms'); fs.chmodSync(fileName, '755'); done(); });
         res.pipe(file);
     } else if ( res.statusCode === 404 ) {
         console.error('Wish binary not found '+wishBinaryUrl);
@@ -93,4 +122,5 @@ https.get(wishBinaryUrl, (res) => {
 }).on('error', (e) => {
     console.error('Failed downloading wish binary.', e);
 });
+
 
--- tools/test/sandbox.js
+++ tools/test/sandbox.js
@@ -1,14 +1,32 @@
 var Mist = require('../../index.js').Mist;
 var Sandboxed = require('../../index.js').Sandboxed;
 
-describe('MistApiSandbox', function () {
+describe('MistApi Sandbox', function () {
     var mist;
     
     before(function (done) {
         mist = new Mist({ name: 'Generic UI', coreIp: '127.0.0.1', corePort: 9094 });
 
-        mist.request('ready', [], function(err, data) {
+        mist.request('ready', [], function(err, ready) {
+            if (ready) {
+                done();
+            } else {
+                done(new Error('MistApi not ready, bailing.'));
+            }
+        });
+    });
+    
+    after(function(done) {
+        mist.shutdown();
+        done();
+    });
+
+
+    it('should get signals', function(done) {
+        mist.request('signals', [], function(err, data) {
+            //console.log("signals", err, data);
             done();
+            done = function() {};
         });
     });
     
@@ -17,8 +35,10 @@ describe('MistApiSandbox', function () {
             if (err) { return done(new Error('wish rpc returned error')); }
             
             if (data.length === 0) {
+                //console.log("Created identity.");
                 mist.wish('identity.create', ['Mr. Andersson'], function(err, data) {
-                    done();
+                    console.log("Wish core had no identities. One has been created. Re-run test.");
+                    process.exit(1);
                 });
             } else {
                 done();
@@ -26,9 +46,159 @@ describe('MistApiSandbox', function () {
         });
     });
     
-    it('should shutdown gracefully', function(done) {
-        mist.shutdown();
-        done();
+    it('should list peers', function(done) {
+        mist.request('listPeers', [], function(err, data) {
+            //console.log('List is this', err, data);
+            done();
+        });
+    });
+    
+    it('shuold test sandbox', function(done) {
+
+        this.timeout(5000);
+
+        mist.request('signals', [], function(err, data) {
+            //console.log("Signal from MistApi", err, data);
+
+            if (data[0] === 'sandboxed.settings') {
+                //console.log("Got request to open settings for a sandbox:", data[1]);
+            } else if (data[0] === 'sandboxed.login') {
+                //console.log("Someone logged in to a sandbox... updating list.");
+
+                mist.request('sandbox.list', [], function(err, data) {
+                    //console.log("Sandbox list reponse:", err, data);
+
+                    for (var i in data) {
+                        if ( Buffer.compare(data[i].id, gpsSandboxId) === 0 ) {
+                            //console.log("Found the GpsApp sandbox for adding a peer:", data[i]);
+
+                            var sandboxId = data[i].id; 
+
+                            mist.request('listPeers', [], function(err, data) {
+                                var peers = [];
+                                for(var i in data) {
+                                    peers.push(data[i]);
+                                }
+
+                                //console.log("Peers available:", peers.length);
+
+                                for(var x in peers) {
+                                    //console.log("Requesting to add peer.", peers[x].rsid.toString());
+                                    if ( peers[x].rsid.slice(0,11).toString() !== 'GPS node.js' ) { continue; }
+                                    mist.request('sandbox.addPeer', [sandboxId, peers[x]], function(err, data) {
+                                        //console.log("Sandbox addPeer reponse:", err, data);
+                                    });
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
+
+        var gpsSandboxId = new Buffer('dead00ababababababababababababababababababababababababababababab', 'hex');
+        var controlThingsSandboxId = new Buffer('beef00ababababababababababababababababababababababababababababab', 'hex');
+
+        var sandboxedGps = new Sandboxed(mist, gpsSandboxId);
+        var sandboxedControlThings = new Sandboxed(mist, controlThingsSandboxId);
+
+        sandboxedGps.request('login', ['Gps App'], function(err, data) {
+            //console.log("Sandbox login reponse:", err, data);
+
+            mist.request('sandbox.list', [], function(err, data) {
+                //console.log("Sandbox list reponse:", err, data);
+            });
+        });
+
+        sandboxedControlThings.request('login', ['ControlThings App'], function(err, data) {
+            //console.log("ControlThings Sandbox login reponse:", err, data);
+
+            sandboxedControlThings.request('listPeers', [], function(err, peers) {
+                //console.log("ControlThings peers:", err, peers);
+            });
+        });
+
+        function peers(err, data) {
+            //console.log("GpsApp sandboxed listPeers reponse:", err, data);
+
+            for (var i in data) {
+                (function (peer) {
+                    if (peer.online) {
+                        //console.log("issuing sandboxed.mist.control.model for", peer);
+                        sandboxedGps.request('mist.control.model', [peer], function (err, model) {
+                            //console.log("Got a model:", err, model);
+
+                            var end = false;
+
+                            var followId = sandboxedGps.request('mist.control.follow', [peer], function (err, data) {
+                                if (!end) {
+                                    end = true;
+                                    sandboxedGps.requestCancel(followId);
+
+                                    sandboxedGps.request('logout', [], function (err, success) {
+                                        //console.log("Sandbox logout reponse:", err, success);
+
+                                        mist.request('sandbox.list', [], function (err, data) {
+                                            console.log("Calling mist.shutdown().");
+                                            mist.shutdown();
+                                            process.nextTick(function() { console.log('exiting.'); process.exit(0); });
+                                            done();
+                                        });
+                                    });
+                                }
+                            });
+                        });
+                    }
+                })(data[i]);
+            }
+        };
+
+        sandboxedGps.request('signals', [], function(err, data) {
+            //console.log("Signal from sandbox", err, data);
+
+            if (data === 'peers') {
+                sandboxedGps.request('listPeers', [], peers);
+            }
+        });
+
+        sandboxedGps.request('listPeers', [], peers);
+
+        sandboxedGps.request('settings', [{ hint: 'commission' }], function(err, data) {
+            //console.log("sandbox settings response", err, data);
+        });
+
+        /*
+        setTimeout(function() {
+            mist.request('sandbox.list', [], function(err, data) {
+                //console.log("Sandbox list reponse:", err, data);
+
+                for (var i in data) {
+                    if ( Buffer.compare(data[i].id, gpsSandboxId) === 0 ) {
+                        //console.log("Found the Gps sandbox:", data[i]);
+                        mist.request('sandbox.listPeers', [gpsSandboxId], function (err, data) {
+
+                            // delete a single peer
+
+                            for(var i in data) {
+                                //console.log("Mist sandbox found a peer to delete:", err, data);
+                                mist.request('sandbox.removePeer', [gpsSandboxId, data[i]], function (err, data) {
+                                    //console.log("Sandbox list reponse:", err, data);
+
+                                    mist.request('sandbox.listPeers', [gpsSandboxId], function (err, data) {
+                                        //console.log("Sandbox listPeers after remove:", err, data);
+                                    });                            
+                                });
+                                return;
+                            }
+
+                            //console.log("No peer to delete.");
+
+                        });
+                    }
+                }
+            });
+        }, 1000);
+        */
     });
     
 });
