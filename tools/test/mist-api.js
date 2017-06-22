2c8e39660c8da09250d8f05b15123cf2ec6583a1
André Kaustell <andre.kaustell@controlthings.fi>
2017-06-22 13:06:45 +0300

    Minor fixes to mist-api test.

--- tools/test/mist-api.js
+++ tools/test/mist-api.js
@@ -6,7 +6,7 @@ describe('MistApi Control', function () {
     var mist;
     
     before(function (done) {
-        mist = new Mist({ name: 'Generic UI', coreIp: '127.0.0.1', corePort: 9094 });
+        mist = new Mist({ name: 'Generic UI' }); // , coreIp: '127.0.0.1', corePort: 9094
 
         setTimeout(function() {
             mist.request('signals', [], function(err, data) {
@@ -14,17 +14,11 @@ describe('MistApi Control', function () {
             });
         }, 200);
     });
-    
-    after(function(done) {
-        process.nextTick(function() { mist.shutdown(); });
-        done();
-    });
 
     it('should get version string', function(done) {
         mist.request('version', [], function(err, data) { 
             if (err) { return done(new Error(inspect(data))); }
             
-            console.log("mist-api version string:", err, data);
             done();
         });
     });
