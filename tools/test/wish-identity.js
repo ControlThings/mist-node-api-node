2a4be674cc92d77f95e0e25271cf309e1f1abfac
Jan Nyman <jan.nyman@controlthings.fi>
2017-08-04 14:46:51 +0300

    Added test for valid transports in locally created identities

--- tools/test/wish-identity.js
+++ tools/test/wish-identity.js
@@ -49,7 +49,7 @@ describe('MistApi Identity', function () {
             var uid = data.uid;
             app.request('identity.get', [uid], function (err, data) {
                 if(err) { if (data.code === 997) { return done(new Error('identity.get returned '+data.code)); } }
-
+                   
                 app.request('identity.remove', [data.uid], function (err, data) {
                     if(err) { if (data.code === 997) { return done(new Error('identity.remove returned '+data.code)); } }
 
@@ -58,4 +58,42 @@ describe('MistApi Identity', function () {
             });
         });
     });
+    
+    it('should create identity with valid transport', function(done) {
+        app.request('identity.create', ['Madame de Pompadour'], function(err, data) {
+            var uid = data.uid;
+            app.request('identity.get', [uid], function (err, data) {
+                if(err) { if (data.code === 997) { return done(new Error('identity.get returned '+data.code)); } }
+                 
+                for (var i in data.hosts) {
+                    var o = data.hosts[i];
+                    if (Array.isArray(o.transports)) {
+                        //console.log("transports:", o.transports);
+
+                        /* Test success criterion: There must be at least one transport, and it must be string starting with: wish */
+                        var url = o.transports[0];
+
+                        if (typeof(url) === "string") {
+                            if (url.startsWith("wish")) {
+                                done();
+                                return;
+                            }
+                            else {
+                                console.log("url is", url);
+                                return done(new Error("Transport url is malformed in identity.get(Bob.uid) from Alice, does not start with 'wish': " + url));
+                            }
+                        } else {
+                            return done(new Error("Transport url is not string in identity.get(Bob.uid) from Alice"));
+                        }
+
+
+                    } else {
+                        return done(new Error("There is no transports array in identity.get(Bob.uid) from Alice"));
+                    }
+                }
+                
+            });
+        });
+    });
+    
 });
