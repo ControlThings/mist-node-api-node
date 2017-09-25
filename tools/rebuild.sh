24e2ce3a6fb9099e0c7cbbf1483088efd041c242
André Kaustell <andre.kaustell@controlthings.fi>
2017-09-25 16:45:34 +0300

    Fixed rebuild script to work correctly on ia32-linux.

--- tools/rebuild.sh
+++ tools/rebuild.sh
@@ -1,2 +1,5 @@
+ARCH=`node -e "console.log(process.arch+'-'+process.platform)"`
+
+echo "Building for ${ARCH}" 
 cd ../mist-c99; rm -rf build; rm libmist.a; make -f make-linux-static-library-x64.mk; cd ../tools;
-cd ..; node-gyp --debug rebuild; cd tools; cp ../build/Debug/MistApi.node ../bin/MistApi-x64-linux.node 
+cd ..; node-gyp --debug rebuild; cd tools; cp ../build/Debug/MistApi.node ../bin/MistApi-${ARCH}.node 
