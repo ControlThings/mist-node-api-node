8c30800b56a257ac4a119d2ab02c3b78b1b07c5f
André Kaustell <andre.kaustell@controlthings.fi>
2017-09-28 17:10:50 +0300

    Added example code and api.js containing new api structure idea.

---
+++ api.js
@@ -0,0 +1,51 @@
+
+var Plugin = {
+    emits: ['online', 'offline', 'frame', 'write', 'invoke', 'wish', 'mist', 'mistnode', 'sandboxed', 'done'],
+    request: fn(['kill', 'wish', 'mist', 'mistnode', 'sandboxed'], BSON(args))
+};
+
+var WishApp = {
+    type: 4,
+    emits: ['ready', 'online', 'offline', 'frame', 'wish'],
+    request: fn(op, args, cb), // 'wish', BSON({ op, args, id })
+    requestBare: fn(op, args, cbBare),
+    requestCancel: fn(id),
+    cancel: fn(id),
+    send: fn(peer, payload),
+    broadcast: fn(payload),
+    disconnect: fn(),
+    shutdown: fn()
+};
+
+var Mist = {
+    type: 2,
+    emits: ['ready'],
+    node: MistNode,
+    wish: WishApp,
+    sandboxes: [Sandboxed-1, Sandboxed-2, '...'],
+    request: fn(op, args, cb),
+    requestBare: fn(op, args, cbBare),
+    requestCancel: fn(id),
+    registerSandbox: fn(Sandboxed),
+    shutdown: fn()
+};
+
+var MistNode = {
+    type: 3,
+    emits: ['ready', 'online', 'offline', 'read', 'write', 'invoke'],
+    wish: WishApp,
+    request: fn(peer, op, args, cb),
+    requestBare: fn(peer, op, args, cbBare),
+    requestCancel: fn(id),
+    create: fn(model),
+    update: fn(endpoint),
+    shutdown: fn()
+};
+
+var Sandboxed = {
+    sandboxId: sid,
+    request: fn(peer, op, args, cb), // { mist.request('sandboxed.'+ op ...) },
+    requestBare: fn(peer, op, args, cbBare),
+    requestCancel: fn(id)
+};
+
