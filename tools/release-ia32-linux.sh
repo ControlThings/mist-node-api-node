7292e116a0f1e23366830a635abf06fa13872997
André Kaustell <andre.kaustell@controlthings.fi>
2017-12-22 14:23:03 +0200

    Updated build scripts for release. release-* scripts under tools.

---
+++ tools/release-ia32-linux.sh
@@ -0,0 +1,14 @@
+#Version of rebuild.sh with explicit compilation with gcc-4.8
+#You must install gcc-4.8 and g++-4.8 
+
+#ARCH=`node -e "console.log(process.arch+'-'+process.platform)"`
+ARCH="ia32-linux"
+
+echo "Building for ${ARCH}" 
+export CFLAGS=-m32
+cd ../mist-c99; rm -rf build; rm libmist.a; make CC=gcc-4.8 -f make-linux-static-library-x64.mk; cd ../tools;
+cd ..; CC=gcc-4.8 CXX=g++-4.8 node-gyp rebuild ; 
+cd tools;
+cp ../build/Release/MistApi.node ../bin/MistApi-${ARCH}.node;
+strip ../bin/MistApi-${ARCH}.node;
+
---
+++ tools/release-raspberry.sh
@@ -0,0 +1,10 @@
+#Version of rebuild.sh with explicit compilation with gcc-4.8
+#You must install gcc-4.8 and g++-4.8 
+
+#ARCH=`node -e "console.log(process.arch+'-'+process.platform)"`
+ARCH="arm-linux"
+
+echo "Building for ${ARCH}" 
+cd ../mist-c99; rm -rf build; rm libmist.a; make CC=arm-linux-gnueabihf-gcc-4.8 -f make-linux-static-library-arm.mk; cd ../tools;
+cd ..; CC=arm-linux-gnueabihf-gcc-4.8 CXX=arm-linux-gnueabihf-g++-4.8 node-gyp rebuild --arch=armv7; cd tools; cp ../build/Release/MistApi.node ../bin/MistApi-${ARCH}.node; arm-linux-gnueabihf-strip ../bin/MistApi-${ARCH}.node; 
+
