59ab779f3da9ef49182dcdc36eaf5c24cae834fa
André Kaustell <andre.kaustell@controlthings.fi>
2017-12-27 14:24:58 +0200

    Added mist-c99 to npmignore and rebuilt ia32 plugin. Update mist-c99. Updated release-ia32 build script.

--- tools/release-ia32-linux.sh
+++ tools/release-ia32-linux.sh
@@ -6,8 +6,8 @@ ARCH="ia32-linux"
 
 echo "Building for ${ARCH}" 
 export CFLAGS=-m32
-cd ../mist-c99; rm -rf build; rm libmist.a; make CC=gcc-4.8 -f make-linux-static-library-x64.mk; cd ../tools;
-cd ..; CC=gcc-4.8 CXX=g++-4.8 node-gyp --release rebuild ; 
+cd ../mist-c99; rm -rf build; rm libmist.a; make CC=gcc-4.8 -f make-linux-static-library-ia32.mk; cd ../tools;
+cd ..; CC=gcc-4.8 CXX=g++-4.8 node-gyp --release rebuild --arch=ia32; 
 cd tools;
 cp ../build/Release/MistApi.node ../bin/MistApi-${ARCH}.node;
 strip ../bin/MistApi-${ARCH}.node;
