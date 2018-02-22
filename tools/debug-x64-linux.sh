42a76ace29183502e00c0cf792ccfff71e12ef57
Jan Nyman <jan.nyman@controlthings.fi>
2018-02-22 15:46:33 +0200

    Default to system default CC and CXX when compiling with gyp debug build on x64 linux

--- tools/debug-x64-linux.sh
+++ tools/debug-x64-linux.sh
@@ -4,7 +4,7 @@ ARCH=`node -e "console.log(process.arch+'-'+process.platform)"`
 
 echo "Building for ${ARCH}" 
 cd ../mist-c99; rm -rf build; rm libmist.a; make CC=gcc -f make-linux-static-library-x64.mk; cd ../tools;
-cd ..; CC=clang CXX=clang node-gyp --debug rebuild; 
+cd ..; node-gyp --debug rebuild; 
 cd tools;
 cp ../build/Debug/MistApi.node ../bin/MistApi-${ARCH}.node;
 
