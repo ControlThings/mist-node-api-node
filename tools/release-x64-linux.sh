bdc1ff25553c7bb8593ce2bb41dcb1ea59c9add7
Jan Nyman <jan.nyman@controlthings.fi>
2018-09-10 11:10:50 +0300

    CC=gcc, CXX=g++ instead for clang for linux x64 release

--- tools/release-x64-linux.sh
+++ tools/release-x64-linux.sh
@@ -4,8 +4,8 @@ ARCH=`node -e "console.log(process.arch+'-'+process.platform)"`
 
 echo "Building for ${ARCH}" 
 cd ../mist-c99; rm -rf build; rm libmist.a; make CC=gcc -f make-linux-static-library-x64.mk; cd ../tools;
-cd ..; CC=clang CXX=clang node-gyp rebuild ; 
-#cd ..; CC=gcc CXX=g++ node-gyp rebuild ; 
+#cd ..; CC=clang CXX=clang node-gyp rebuild ; 
+cd ..; CC=gcc CXX=g++ node-gyp rebuild ; 
 cd tools;
 cp ../build/Release/MistApi.node ../bin/MistApi-${ARCH}.node;
 strip ../bin/MistApi-${ARCH}.node;
