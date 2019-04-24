5caaf3c6b279d153ad4be84439a58a895e26a2aa
Jan Nyman <jan.nyman@controlthings.fi>
2019-04-24 21:42:33 +0300

    Build new libs for Darwin/macOS

--- tools/release-x64-darwin.sh
+++ tools/release-x64-darwin.sh
@@ -1,7 +1,7 @@
-ARCH=`node -e "console.log(process.arch+'-'+process.platform)"`
+ARCH_VERSION=`node -e "console.log(process.arch + '-' + process.platform + '-' + process.version.split('.')[0])"`
 
-echo "Building for ${ARCH}" 
+echo "Building for ${ARCH_VERSION}" 
 cd ../mist-c99; rm -rf build; rm libmist.a; make -f make-linux-static-library-x64-darwin.mk; cd ../tools;
 cd ..; node-gyp --release rebuild; cd tools;
-cp ../build/Release/MistApi.node ../bin/MistApi-${ARCH}.node; 
-strip ../bin/MistApi-${ARCH}.node
+cp ../build/Release/MistApi.node ../bin/MistApi-${ARCH_VERSION}.node; 
+strip ../bin/MistApi-${ARCH_VERSION}.node
