101502eafc63e4d4b0be14bd611f0e61e34e7f3f
Jan Nyman <jan.nyman@controlthings.fi>
2019-05-21 10:18:50 +0300

    Improved debug build script

--- tools/debug-x64-linux.sh
+++ tools/debug-x64-linux.sh
@@ -2,13 +2,13 @@
 
 ARCH_VERSION=`node -e "console.log(process.arch + '-' + process.platform + '-' + process.version.split('.')[0])"`
 
-echo "Building for ${ARCH_VERSION}" 
+echo "Building debug build for ${ARCH_VERSION}" 
 # WARNING: Do not override CFLAGS here, as it will totally override definition mist-c99 makefile.
 cd ../mist-c99; make -f make-static-library.mk clean; make CC=gcc BUILD_TYPE=nodejs_plugin BUILD_FLAVOUR=debug VERBOSE=1 -f make-static-library.mk; cd ../tools;
 
 #cd ..; CC=clang CXX=clang node-gyp rebuild ; 
-cd ..; CC=gcc CXX=g++ CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 node-gyp rebuild ; 
+cd ..; CC=gcc CXX=g++ CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 node-gyp --debug rebuild ; 
 cd tools;
-cp ../build/Release/MistApi.node ../bin/MistApi-${ARCH_VERSION}.node;
-strip ../bin/MistApi-${ARCH_VERSION}.node;
+cp ../build/Debug/MistApi.node ../bin/MistApi-${ARCH_VERSION}.node;
+
 
