0895d79dbdb1d88d6fd8746e16ca8dc635c82ad3
Jan Nyman <jan.nyman@controlthings.fi>
2019-05-21 11:04:56 +0300

    Generic build script for producing a debug build

---
+++ tools/debug-build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Build a debug build. 
+# The build script uses CC and CXX environment variables. If they are not defined, gcc and g++ are defaulted to.
+# 
+# Example on Linux: CC=gcc CXX=g++ ./debug-x64-linux.sh
+# Example on macOS: CC=clang CXX=clang ./debug-x64-linux.sh
+# 
+
+ARCH_VERSION=`node -e "console.log(process.arch + '-' + process.platform + '-' + process.version.split('.')[0])"`
+
+if [ -z $CC ]; then CC=gcc; fi
+
+if [ -z $CXX ]; then CXX=g++; fi
+
+echo "Building debug build for ${ARCH_VERSION}" 
+# WARNING: Do not override CFLAGS here, as it will totally override definition mist-c99 makefile.
+cd ../mist-c99; make -f make-static-library.mk clean; make CC=$CC BUILD_TYPE=nodejs_plugin BUILD_FLAVOUR=debug VERBOSE=1 -f make-static-library.mk; cd ../tools;
+
+cd ..; CC=$C CXX=$CXX node-gyp --debug rebuild ; 
+cd tools;
+
+# The library is in build/Debug/MistApi.node
