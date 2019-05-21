9dbf642da098e31600bf08be72f5008f495582a3
Jan Nyman <jan.nyman@controlthings.fi>
2019-05-21 16:30:55 +0300

    Update IA32 build script

--- tools/release-ia32-linux.sh
+++ tools/release-ia32-linux.sh
@@ -6,9 +6,9 @@ ARCH_VERSION=`node -e "console.log('ia32-linux-' + process.version.split('.')[0]
 
 echo "Cross-building for ${ARCH}" 
 
-cd ../mist-c99; make -f make-static-library.mk clean; make CC=gcc APPEND_CFLAGS=-m32  BUILD_TYPE=nodejs_plugin BUILD_FLAVOUR=release -f make-static-library.mk; cd ../tools;
+cd ../mist-c99; make -f make-static-library.mk clean; make CC=gcc APPEND_CFLAGS="-m32 -std=gnu99"  BUILD_TYPE=nodejs_plugin BUILD_FLAVOUR=release -f make-static-library.mk; cd ../tools;
 cd ..; CC=gcc CXX=g++ node-gyp --release rebuild --arch=ia32; 
 cd tools;
-cp ../build/Release/MistApi.node ../bin/MistApi-${ARCH}.node;
-strip ../bin/MistApi-${ARCH}.node;
+cp ../build/Release/MistApi.node ../bin/MistApi-${ARCH_VERSION}.node;
+strip ../bin/MistApi-${ARCH_VERSION}.node;
 
