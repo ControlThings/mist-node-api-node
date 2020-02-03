09f05bb63f6b0eae77d0ade681311754a05932c7
Jan Nyman <jan.nyman@controlthings.fi>
2020-02-03 16:37:05 +0200

    Add copyright banners

--- tools/test/mist-wrapper-request-flood.js
+++ tools/test/mist-wrapper-request-flood.js
@@ -1,4 +1,15 @@
 /**
+ * Copyright (C) 2020, ControlThings Oy Ab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @license Apache-2.0
+ */
+/**
  * This test tests the Mist node.js wrapper in the situation where a very large number of requests are send to the core simulaneously.
  */
 
